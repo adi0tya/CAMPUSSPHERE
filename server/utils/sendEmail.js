da550a584b2dd13ee83e3bbdf3f71baf9f04171f
Aditya Das <ad508042@gmail.com>
2026-05-15 11:33:36 +0530

    fix: force secure port 465 and add 10s timeout to nodemailer

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,11 +3,16 @@ const nodemailer = require('nodemailer');
 const sendEmail = async ({ email, subject, message, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // You can change this to any other service
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      }
+      },
+      connectionTimeout: 10000, // 10 seconds timeout
+      greetingTimeout: 10000,
+      socketTimeout: 10000
     });
 
     const mailOptions = {
